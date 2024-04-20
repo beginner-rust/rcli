db7edea0db819cb117d11c7b37d329145b12dc05
beginner-rust <wind614@163.com>
2024-04-20 21:06:23 +0800

    save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(name="rcli",version,author,about,long_about=None)]
+struct Opts {
+    #[command(subcommand)]
+    cmd: SubCommand,
+
+}
+
+#[derive(Parser, Debug)]
+enum SubCommand {
+    #[command(name="csv",about ="show csv,or convert csv to other fomart")]
+    Csv(CvsOpts),
+}
+
+#[derive(Parser, Debug)]
+struct CvsOpts {
+
+    #[arg(short,long)]
+    input: String,
+
+    #[arg(short,long,default_value="output.json")]
+    output: String,
+
+    #[arg(short,long,default_value_t = ',')]
+    delimiter: char,
+
+    #[arg(short,long,default_value_t = true)]
+    header: bool,
+
+}
+
 fn main() {
-    println!("Hello, world!!!");
+    let opts = Opts::parse();
+    panic!("{:?}",opts)
 }
